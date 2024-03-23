744047394539a712acd722d2d15403b9fbcc9065
Oleksandr Zozulia <freemanlex@gmail.com>
2024-03-23 20:45:16 +0200

    "first commit"

---
+++ verification/tests.py
@@ -0,0 +1,61 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ["ACEGIKMOQSUW"],
+            "answer": "YACEGIKMOQSU",
+        },
+        {
+            "input": ["AQBRCSDTEUFV"],
+            "answer": "GWHXIYJZKALB",
+        },
+        {
+            "input": ["AZTAYRAXPAWN"],
+            "answer": "AVLAUJATHASF",
+            "explanation": "1+2=?"
+        },
+    ],
+    "Extra": [
+        {
+            "input": ["AZYXWVUTSRQP"],
+            "answer": "ONMLKJIHGFED",
+        },
+        {
+            "input": ["AAAAAAAAAAAA"],
+            "answer": "AAAAAAAAAAAA",
+        },
+        {
+            "input": ["CGAIYKWMUOSQ"],
+            "answer": "QSOUMWKYIAGC",
+        },
+        {
+            "input": ["STZQGNNKUHBE"],
+            "answer": "IBPYWVDSKPRM",
+        },
+        {
+            "input": ["XXYHZRABBLCV"],
+            "answer": "DFEPFZGJHTID",
+        },
+        {
+            "input": ["STGZQHGNINKJ"],
+            "answer": "UHKBELIBMPYN",
+        },
+        {
+            "input": ["KPUPUZUZEZEJ"],
+            "answer": "EJOJOTOTYTYD",
+        },
+        {
+            "input": ["CEGBCDAAAZYX"],
+            "answer": "YWUXURWSOVQL",
+        },
+    ]
+}
